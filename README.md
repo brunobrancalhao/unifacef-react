4313ac269634f670146750b7f706c936ae28d8e9
Bruno Brancalhao <brunobrancalhao26@gmail.com>
2020-06-06 14:39:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Unifacef React TypeScript
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/08a8996e-2d68-4a0d-9196-c36257025f08/deploy-status)](https://app.netlify.com/sites/luindayk-unifacef-react-frontend/deploys)
---
+++ src/routes/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+import { observer } from 'mobx-react';
+import { withRouter, Switch, Route } from 'react-router-dom';
+import { Divider } from 'semantic-ui-react';
+
+import { endpoints } from './endpoints';
+import NotFound from '../containers/not-found';
+import MainMenu from '../components/main-menu';
+
+// @ts-ignore
+@withRouter
+@observer
+export default class Routes extends React.Component {
+
+  render() {
+    return(
+      <>
+        <MainMenu />
+        <Divider hidden={true}></Divider>
+        <Switch>
+          {endpoints.map((route, key) => (
+            <Route key={key} {...route} />
+          ))}
+          <Route path="*" exact={true} render={props => <NotFound {...props} />} />
+        </Switch>
+      </>
+    );
+  }
+
+}
