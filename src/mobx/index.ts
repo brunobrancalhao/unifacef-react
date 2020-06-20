bd414f831e067bd8884138c3e5575de4fe256a71
Bruno Brancalhao <brunobrancalhao26@gmail.com>
2020-06-20 11:36:24 -0300

    Changes starwars

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -1,9 +1,13 @@
 import { combustivel } from '../containers/combustivel/store';
 import { home } from '../containers/home/store';
 import { router } from './router.store';
+import { starWars } from '../containers/star-wars/store';
+import { starWarsDetails } from '../containers/star-wars-details/store';
 
 export {
   router,
   combustivel,
-  home
+  home,
+  starWars,
+  starWarsDetails,
 }
--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -2,6 +2,8 @@ import Combustivel from "../containers/combustivel";
 import Home from "../containers/home";
 import { RouteProps } from "react-router-dom";
 import Sobre from "../containers/sobre";
+import StarWars from '../containers/star-wars';
+import StarWarsDetails from "../containers/star-wars-details";
 
 interface EndPointsProps extends RouteProps {
   name?: string;
@@ -13,5 +15,7 @@ export const endpoints: EndPointsProps[] = [
   {path: `${publicUrl}/`, component: Home, exact: true},
   {path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true},
   {path: `${publicUrl}/combustivel`, name: 'Combustivel', component: Combustivel, exact: true},
+  {path: `${publicUrl}/star-wars`, name: 'Star Wars', component: StarWars, exact: true},
+  {path: `${publicUrl}/star-wars/:id`, component: StarWarsDetails, exact: true},
   {path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true},
 ];
